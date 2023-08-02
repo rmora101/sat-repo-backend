c691356eb9917b99fd2b6fb026cb5b10f3796250
rmora101 <59456883+rmora101@users.noreply.github.com>
2023-08-02 13:56:57 -0700

    routes updated for city populations.

---
+++ app/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+
+app.use(express.static('js'));
+
+app.listen(3000, () => {
+  console.log("Application started and Listening on port 3000");
+});
+app.get('/', (req, res) => {
+    res.send('home')
+  })
+
--- app/routes.js
+++ app/routes.js
@@ -4,23 +4,23 @@ const authToken = process.env.CENSUS_TOKEN;
 
 
 
-// axios.get("https://api.census.gov/data/2016/acs/acs1?get=NAME&for=state:*")
-//     .then((response) => {
-//         for (let data of response.data) {
-//             const name = data[0];   // Accessing the "NAME" field
-//             const state = data[1];  // Accessing the "state" field
-//             console.log(`Name: ${name}, State: ${state}`);
-//         }
-//     })
-//     .catch(console.error);
+axios.get("https://api.census.gov/data/2016/acs/acs1?get=NAME&for=state:*")
+    .then((response) => {
+        for (let data of response.data) {
+            const name = data[0];   // Accessing the "NAME" field
+            const state = data[1];  // Accessing the "state" field
+            console.log(`Name: ${name}, State: ${state}`);
+        }
+    })
+    .catch(console.error);
 
 axios.get("https://api.census.gov/data/2021/acs/acs1?get=NAME,B01001_001E&for=public%20use%20microdata%20area:*&in=state:04")
     .then((response) => {
         for (let data of response.data) {
             const name = data[0];   // Accessing the "NAME" field
             const population = data[1];  // Accessing the "state" number field
-            const state = data[2];
-            const microDataArea = data[3];
+            const state = data[2]; // access state code
+            const microDataArea = data[3]; // access micro area code
             console.log(`NAME: ${name}, STATE: ${state}, POPULATION:${population}, CITY_CODE:${microDataArea}`);
         }
     })
