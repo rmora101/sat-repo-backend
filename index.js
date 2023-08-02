efab5c2de91b424c412fa77aabcbeb6a2380adae
rmora101 <59456883+rmora101@users.noreply.github.com>
2023-08-02 13:48:34 -0700

    added the get population of cities function

--- index.js
+++ index.js
@@ -19,16 +19,16 @@ census({
 (err, res) => console.log(res)
 )
 // median household income sdk calls
-census({
-    vintage: '2021',
-    geoHierarchy: {
-        state:'04',
-        'metropolitan statistical area/micropolitan statistical area': '*',
-    },
-    sourcePath: ['acs', 'acs1','profile'],
-    values: ['DP03_0062E'], // GINI index
-    statsKey: apiAccessKey,
-    geoResolution: '500k',
-},   
-(err, res) => console.log(res)
-)
+// census({
+//     vintage: '2021',
+//     geoHierarchy: {
+//         state:'04',
+//         'metropolitan statistical area/micropolitan statistical area': '*',
+//     },
+//     sourcePath: ['acs', 'acs1','profile'],
+//     values: ['DP03_0062E'], // GINI index
+//     statsKey: apiAccessKey,
+//     geoResolution: '500k',
+// },   
+// (err, res) => console.log(res)
+// )
